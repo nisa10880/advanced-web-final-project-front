90b70a48fc4f8a95a47da34875fa9d40691ea006
thib92 <thibhenry92@gmail.com>
2020-01-26 17:04:30 +0100

    Move add cards to top

--- src/Home.js
+++ src/Home.js
@@ -65,6 +65,21 @@ export const Home = () => {
 
   return (
     <Grid container spacing={4}>
+      <Grid item xs={12} sm={12} md={4}>
+        <Panel title="Ajouter des points">
+          <AddPoints />
+        </Panel>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Panel title="Ajouter un élève">
+          <AddStudent />
+        </Panel>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Panel title="Ajouter un professeur">
+          <AddProfessor />
+        </Panel>
+      </Grid>
       <Grid item xs={12} sm={6}>
         <Panel title="Maisons">
           <ItemsList
@@ -81,21 +96,6 @@ export const Home = () => {
             extractPrimary={(prof) => `${prof.firstname} ${prof.lastname}`}
             extractSecondary={(prof) => prof.points}
           />
-        </Panel>
-      </Grid>
-      <Grid item xs={12} sm={12} md={4}>
-        <Panel title="Ajouter des points">
-          <AddPoints />
-        </Panel>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Panel title="Ajouter un élève">
-          <AddStudent />
-        </Panel>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Panel title="Ajouter un professeur">
-          <AddProfessor />
         </Panel>
       </Grid>
     </Grid>
