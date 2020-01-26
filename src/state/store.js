225bec0aa030edbc256bb23df46e388334dbaa25
thib92 <thibhenry92@gmail.com>
2020-01-26 16:54:55 +0100

    Fix Redux Devtools

--- src/state/store.js
+++ src/state/store.js
@@ -17,13 +17,12 @@ const reducers = combineReducers({
   snackbar: snackbarReducer,
 });
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const store = createStore(
   reducers,
-  compose(
-    applyMiddleware(
-      thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(thunk)
   )
 );
 
